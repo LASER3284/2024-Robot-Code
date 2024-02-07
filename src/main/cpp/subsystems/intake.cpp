282b38df2049c07023681a2fa71b43ddd04fe510
Keith Bass <keithrb13@gmail.com>
2024-02-07 15:48:10 -0600

    Intake mechanism changed, deploy stuff removed.
    Constant values are still needed and everything still needs testing.

--- src/main/cpp/subsystems/intake.cpp
+++ src/main/cpp/subsystems/intake.cpp
@@ -4,50 +4,10 @@
 using namespace subsystems;
 
 void intake::Intake::init() {
-    deploy_motor.SetIdleMode(rev::CANSparkMax::IdleMode::kBrake);
-    deploy_motor.SetInverted(false);
-
     roller_motor.SetInverted(true);
-
-    // Value given in degrees
-    deploy_controller.SetTolerance(1.0);
-    velocity_timer.Start();
-}
-
-void intake::Intake::update_nt() {
-    // Must be the first thing
-    units::degree_t dtheta = previous_angle - get_angle();
-    units::second_t dt = previous_time - velocity_timer.Get();
-    deploy_velocity = dtheta / dt;
-
-    // Stuff in middle
-
-    // Must be the last thing
-    previous_angle = get_angle();
-    previous_time = velocity_timer.Get();
 }
 
 void intake::Intake::tick() {
-    frc::TrapezoidProfile<units::degrees> rot_profile {
-        deploy_constraints,
-        deploy_goal,
-        deploy_setpoint
-    };
-
-    deploy_setpoint = rot_profile.Calculate(20_ms);
-
-    units::volt_t deploy_voltage = deploy_ff.Calculate(
-        deploy_setpoint.position,
-        deploy_setpoint.velocity
-    ) + units::volt_t{deploy_controller.Calculate(
-        get_angle().value(),
-        deploy_setpoint.position.value()
-    )};
-
-    frc::SmartDashboard::PutNumber("intake_deploy_v", deploy_voltage.value());
-
-    deploy_motor.SetVoltage(deploy_voltage);
-
     roller_motor.SetVoltage(roller_voltage);
 }
 
@@ -57,62 +17,4 @@ void intake::Intake::activate(intake::constants::DeployStates state) {
     else {
         roller_voltage = intake::constants::ROLLER_INTAKE_SETPOINT;
     }
-
-    if (state > intake::constants::DeployStates::DOWN_SPIN) {
-        deploy_goal = {intake::constants::UPPER_LIMIT, 0_deg_per_s};
-    } else {
-        deploy_goal = {intake::constants::LOWER_LIMIT, 0_deg_per_s};
-    }
-}
-
-void intake::Intake::set_deploy(units::volt_t volts) {
-    deploy_motor.SetVoltage(volts);
-}
-
-units::volt_t intake::Intake::get_deploy_volts() {
-    return deploy_motor.Get() * frc::RobotController::GetBatteryVoltage();
-}
-
-void intake::Intake::set_deploy_goal(units::degree_t angle) {
-    deploy_setpoint = {angle};
-    deploy_goal = {angle};
-}
-
-void intake::Intake::run_sysid(int test_num) {
-    if (sysid_command && !sysid_command->IsScheduled()) {
-        sysid_command->Schedule();
-    } else if (sysid_command && sysid_command->IsScheduled()) {
-        // empty code :)
-        // wait for something else to cancel the scheduled command
-    } else {
-        switch (test_num) {
-        case 0: {
-            sysid_command = sysid.Quasistatic(frc2::sysid::Direction::kForward);
-            break;
-        }
-        case 1: {
-            sysid_command = sysid.Quasistatic(frc2::sysid::Direction::kReverse);
-            break;
-        }
-        case 2: {
-            sysid_command = sysid.Dynamic(frc2::sysid::Direction::kForward);
-            break;
-        }
-        case 3: {
-            sysid_command = sysid.Dynamic(frc2::sysid::Direction::kReverse);
-            break;
-        }
-        default: {
-            cancel_sysid();
-            break;
-        }
-        }
-    }
-}
-
-void intake::Intake::cancel_sysid() {
-    if (sysid_command)
-        sysid_command->Cancel();
-
-    sysid_command = std::nullopt;
 }
--- src/main/include/subsystems/intake.h
+++ src/main/include/subsystems/intake.h
@@ -27,28 +27,12 @@ namespace intake {
 
 namespace constants {
     enum DeployStates {
-        DOWN_NOSPIN = 0,
-        DOWN_SPIN,
-        UP_NOSPIN,
-        UP_SPIN
+        NOSPIN = 0,
+        SPIN = 0
     };
 
     /// @brief The CAN ID for the roller motor.
     constexpr int ROLLER_ID = 0;
-    /// @brief The CAN ID for the deploy motor.
-    constexpr int DEPLOY_ID = 0;
-
-    /// @brief Angle offset for the thru-bore (this is subtracted from the reported angle).
-    constexpr units::degree_t THRUBORE_ANGLE_OFFSET = -0_deg;
-    
-    // NOTE: The PID values for the deploy motor are set with max control effort from SysID at 0V.
-
-    /// @brief The proportional gain for the deploy controller.
-    constexpr double DEPLOY_P = 0.0;
-    /// @brief The integral gain for the deploy controller.
-    constexpr double DEPLOY_I = 0.0;
-    /// @brief The derivative gain for the deploy controller.
-    constexpr double DEPLOY_D = 0.0;
 
     /// @brief The statically applied voltage for the roller motor.
     constexpr auto ROLLER_KS = 0.0_V;
@@ -75,9 +59,6 @@ public:
     /// @brief Initializes the motors and ensures break mode on the NEO.
     void init();
 
-    /// @brief Updates SmartDashboard values, should be called in RobotPeriodic().
-    void update_nt();
-
     /// @brief Updates the state of the motors and gives new voltages to feed to
     /// the motors. Should be called every enabled periodic, except
     /// TestPeriodic().
@@ -88,95 +69,13 @@ public:
     /// @param state The desired state of the intake. This can only be applied
     /// through periodic calls to tick().
     void activate(constants::DeployStates);
-
-    /// @brief Calculates the angle of the intake (i.e, whether it's up or down).
-    /// @return The angle in degrees.
-    units::degree_t get_angle() const {
-        return thrubore_enc.Get() + constants::THRUBORE_ANGLE_OFFSET;
-    }
-
-    units::degrees_per_second_t get_angle_vel() {
-        return deploy_velocity;
-    }
-
-    void run_sysid(int);
-
-    void cancel_sysid();
 private:
-    /// @brief Used to find the delta-theta so we can get d-theta/dt.
-    units::degree_t previous_angle;
-
-    units::second_t previous_time;
-
-    units::degrees_per_second_t deploy_velocity;
-
-    frc::Timer velocity_timer {};
-
-    /// @brief Sets the voltage of the deploy motor.
-    /// @param volts The voltage that should be applied to the deploy motor.
-    void set_deploy(units::volt_t volts);
-
-    /// @brief Fetches the amount of voltage applied to the motor as a
-    /// percentage of battery voltage.
-    /// @return See brief :)
-    units::volt_t get_deploy_volts();
-
-    /// @brief Has the side effect of setting the deploy_setpoint and deploy_goal to the specified position.
-    /// @param angle The desired position.
-    /// @warning Bounds checking is not done for this function! Be cautious of its side effects.
-    void set_deploy_goal(units::degree_t angle);
-    
-    /// @brief The deploy motor.
-    rev::CANSparkMax deploy_motor {
-        constants::DEPLOY_ID,
-        rev::CANSparkMaxLowLevel::MotorType::kBrushless
-    };
-
-    /// @brief The encoder that measures the absolute angle of the intake.
-    frc::DutyCycleEncoder thrubore_enc { 0 };
-
-    /// @brief The PID controller for the deploy motor.
-    frc::PIDController deploy_controller {
-        constants::DEPLOY_P,
-        constants::DEPLOY_I,
-        constants::DEPLOY_D
-    };
-
-    frc::ArmFeedforward deploy_ff {0_V, 0_V, 0_V / 1_deg_per_s, 0_V / 1_deg_per_s_sq};
-
-    frc::TrapezoidProfile<units::degrees>::Constraints deploy_constraints {
-        0_deg_per_s,
-        0_deg_per_s_sq
-    };
-
-    frc::TrapezoidProfile<units::degrees>::State deploy_goal;
-
-    frc::TrapezoidProfile<units::degrees>::State deploy_setpoint;
-
     /// @brief The roller motor. 
     ctre::phoenix6::hardware::TalonFX roller_motor {
         constants::ROLLER_ID,
     };
 
     units::volt_t roller_voltage = 0_V;
-
-    frc2::sysid::SysIdRoutine sysid {
-        frc2::sysid::Config {0.25_V / 1_s, 4_V, std::nullopt, std::nullopt},
-        frc2::sysid::Mechanism {
-            [this](units::volt_t volts) {
-                deploy_motor.SetVoltage(volts);
-            },
-            [this](auto log) {
-                log->Motor("intake-deploy-motor")
-                    .voltage(get_deploy_volts())
-                    .velocity(units::turns_per_second_t{get_angle_vel()})
-                    .position(units::turn_t{get_angle()});
-            },
-            this
-        }
-    };
-
-    std::optional<frc2::CommandPtr> sysid_command;
 }; // class intake
 
 } // namespace intake
